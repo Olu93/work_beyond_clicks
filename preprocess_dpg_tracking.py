7c6dd2baada996422e17533ca2465ae057ee0747
Olusanmi Hundogan <o.hundogan@gmail.com>
2022-10-28 14:07:52 +0200

    Cleanup: Code is now cleaned and commented

--- preprocess_dpg_tracking.py
+++ preprocess_dpg_tracking.py
@@ -18,6 +18,7 @@ import gzip
 import collections
 import tqdm
 import io
+import csv
 
 # %%
 load_dotenv()  # take environment variables from .env.
@@ -31,6 +32,7 @@ s3_client = boto3.client(
     aws_secret_access_key=os.environ["SECRET_KEY"],
 )
 
+# FILENAME is not really needed. But remains for example purposes.
 BUCKET_NAME = "recosearch-nps-bucket-shared-with-utrecht-university"
 FILE_NAME = "20220909-test-on-tracking-event-file-partition-and-compression/tracking_events_single_file_compressed_json_2022-06-30/part-00000-d667af8c-fe5d-4c75-b144-400a5560425d-c000.json.gz"
 
@@ -40,116 +42,54 @@ file_list = [pathlib.Path(item["Key"]) for item in s3_objects["Contents"] if "js
 print(file_list)
 
 # %%
-# read a file in json lines format and print its first record
-# Note: the "Body" field contains the data as a botocore.response.StreamingBody object,
-# and .read() will read all the content of the object
-# json_list = s3_object["Body"].read().decode("utf-8").strip().split("\n")
-# print(json.loads(json_list[0]))
-# %%
-# try to delete the bucket - you should get an access denied error because you have read-only permissions
-# s3_client.delete_bucket(Bucket=bucket_name)
-
-# %%
-
-# cnt = 0
-# s3_object = s3_client.get_object(Bucket=bucket_name, Key=FILE_NAME)
-# print("Loading File")
-# s3_file_content = s3_object['Body'].read()
-# print("Loading Complete")
-# with gzip.GzipFile(None, "rb", fileobject=BytesIO(s3_file_content)) as file:
-#     print("Opening Zip-File")
-#     for cnt, l in enumerate(file):
-#         print(l)
-#         if cnt > 10:
-#             break
-
-
-# %%
-# class ProgressPercentage(object):
-#     ''' Progress Class
-#     Class for calculating and displaying download progress
-#     '''
-#     def __init__(self, client, bucket, filename):
-#         ''' Initialize
-#         initialize with: file name, file size and lock.
-#         Set seen_so_far to 0. Set progress bar length
-#         '''
-#         self._filename = filename
-#         self._size = client.head_object(Bucket=bucket, Key=filename)['ContentLength']
-#         self._seen_so_far = 0
-#         self._lock = threading.Lock()
-#         self.prog_bar_len = 80
-
-#     def __call__(self, bytes_amount):
-#         ''' Call
-#         When called, increments seen_so_far by bytes_amount,
-#         calculates percentage of seen_so_far/total file size 
-#         and prints progress bar.
-#         '''
-#         # To simplify we'll assume this is hooked up to a single filename.
-#         with self._lock:
-#             self._seen_so_far += bytes_amount
-#             ratio = round((float(self._seen_so_far) / float(self._size)) * (self.prog_bar_len - 6), 1)
-#             current_length = int(round(ratio))
-
-#             percentage = round(100 * ratio / (self.prog_bar_len - 6), 1)
-
-#             bars = '+' * current_length
-#             output = bars + ' ' * (self.prog_bar_len - current_length - len(str(percentage)) - 1) + str(percentage) + '%'
-
-#             if self._seen_so_far != self._size:
-#                 sys.stdout.write(output + '\r')
-#             else:
-#                 sys.stdout.write(output + '\n')
-#             sys.stdout.flush()
-
-
-# %%
-# s3 = boto3.resource('s3', aws_access_key_id=os.environ["ACCESS_KEY"], aws_secret_access_key=os.environ["SECRET_KEY"])
-# binary_file = BytesIO()
-# progress = ProgressPercentage(s3_client, BUCKET_NAME, FILE_NAME)
-# s3.Bucket(BUCKET_NAME).download_fileobj(FILE_NAME, binary_file, Callback=progress)
-# gzip_file = gzip.GzipFile(None, 'rb', fileobj=binary_file)
-# decompressed = gzip_file.decompress()
-# decompressed
-# %%
-import csv
 counter = collections.defaultdict(int)
 raw_collector = collections.deque()
 view_collector = collections.deque()
 interaction_collector = collections.deque()
-geo_mapping = collections.defaultdict(str)
-city_mapping = collections.defaultdict(lambda: len(city_mapping)+1)
+mapping_geo = collections.defaultdict(str)
+mapping_city = collections.defaultdict(lambda: len(mapping_city)+1)
+mapping_refferer = collections.defaultdict(lambda: len(mapping_refferer)+1)
 limit = 10000
 update_freq = 1000
 
-def reduce_event(geo_mapping, city_mapping, tmp_dict):
+# Helper function for the reduction part.
+def reduce_event(geo_mapping, city_mapping, refferer_mapping, tmp_dict):
+    # As a user can only have either an android id, or an apple id or a privacy wall id, we combine these columns. 
     tmp_dict["QUASI_USER_ID"] = tmp_dict.pop("ANDROID_ID", None) or tmp_dict.pop("APPLE_ID", None) or tmp_dict.pop("PRIVACYWALL_ID", None) or ""
+    # If a user is logged in, the user has a GIGYA-ID. We only save whther the user is logged in or not. 
     tmp_dict["IS_LOGGED_IN"] = (tmp_dict.pop("GIGYA_ID", None) is not None)*1
+    
+    # Creates two look up tables.
+    # Look up for mapping from geo-region-abbreviation to region name and timezone 
     geo_mapping[tmp_dict.pop("GEO_REGION", None)] = {"region_name":tmp_dict.pop("GEO_REGION_NAME", None), "timezone": tmp_dict.pop("GEO_TIMEZONE", None)} 
+    # Look up for mapping from city to id 
     city_mapping[tmp_dict.get("GEO_CITY", None)]    
     tmp_dict["GEO_CITY"] = city_mapping[tmp_dict.get("GEO_CITY", None)]
-            
-    if "DVCE_CREATED_TSTAMP" in tmp_dict:
-        del tmp_dict["DVCE_CREATED_TSTAMP"]
-    if "DOMAIN_USERID" in tmp_dict:
-        del tmp_dict["DOMAIN_USERID"]
-    if "DOMAIN_SESSIONID" in tmp_dict:
-        del tmp_dict["DOMAIN_SESSIONID"]
-    if "REFR_URLHOST" in tmp_dict:
-        del tmp_dict["REFR_URLHOST"]
-    if "PLATFORM" in tmp_dict:
-        del tmp_dict["PLATFORM"]
-    if "PAGE_TITLE" in tmp_dict:
-        del tmp_dict["PAGE_TITLE"]
-    if "PAGE_TYPE" in tmp_dict:
-        del tmp_dict["PAGE_TYPE"]
-    if "PAGE_REFERRER" in tmp_dict:
-        del tmp_dict["PAGE_REFERRER"]
-    if "PAGE_URLHOST" in tmp_dict:
-        del tmp_dict["PAGE_URLHOST"]
-    if "PAGE_URLPATH" in tmp_dict:
-        del tmp_dict["PAGE_URLPATH"]
+    # Look up for mapping from city to id 
+    refferer_mapping[tmp_dict.get("REFR_URLHOST", None)]    
+    tmp_dict["REFR_URLHOST"] = refferer_mapping[tmp_dict.get("REFR_URLHOST", None)]
+    
+    # Removal of unneeded information in the data
+    tmp_dict.pop("DVCE_CREATED_TSTAMP", None) 
+    # DOMAIN USER- and SESSIONID are created by the snow plow system but DPG uses PRIVACYWALL_ID instead.        
+    tmp_dict.pop("DOMAIN_USERID", None)        
+    tmp_dict.pop("DOMAIN_SESSIONID", None)
+    
+    tmp_dict.pop("PLATFORM", None)        
+    # Removal-Reason: Information is already available in the article data.
+    tmp_dict.pop("PAGE_TITLE", None)        
+    # Removal-Reason: Information is already available in the article data.
+    tmp_dict.pop("PAGE_URLHOST", None)        
+    # Removal-Reason: Information is already available in the article data.
+    tmp_dict.pop("PAGE_URLPATH", None)        
+    # Removal-Reason: Type is always 'article' 
+    tmp_dict.pop("PAGE_TYPE", None)        
+    # Removal-Reason: Holds redundant information to field 'REFR_URLHOST'
+    tmp_dict.pop("PAGE_REFERRER", None)        
+    # Removal-Reason: Holds redundant information to field 'REFR_URLHOST'
+    tmp_dict.pop("REFR_SOURCE", None) 
+    
+    # Seperates all combined privacy configurations into binary values.        
     if "PRIVACY_SETTINGS" in tmp_dict:
         psettings = tmp_dict["PRIVACY_SETTINGS"]
         tmp_dict["privacy_functional"] = ("functional" in psettings)*1
@@ -164,16 +104,10 @@ def reduce_event(geo_mapping, city_mapping, tmp_dict):
         del tmp_dict["PRIVACY_SETTINGS"]
     return tmp_dict
 
-def remove_interaction_cols(tmp_dict):
-    if "SE_VALUE" in tmp_dict:
-        del tmp_dict["SE_VALUE"]
-    if "SE_CATEGORY" in tmp_dict:
-        del tmp_dict["SE_CATEGORY"]
-    if "SE_LABEL" in tmp_dict:
-        del tmp_dict["SE_LABEL"]
-    if "SE_ACTION" in tmp_dict:
-        del tmp_dict["SE_ACTION"]
 
+# This is a pre-flight to get all necessary columns beforehand.
+limit = 10000
+update_freq = 1000
 for file_name in file_list:
     pbar = tqdm.tqdm(range(limit), total=limit)
     with gzip.GzipFile(fileobj=s3_client.get_object(Bucket=BUCKET_NAME, Key=str(file_name))["Body"]) as gzipfile:
@@ -182,7 +116,7 @@ for file_name in file_list:
             tmp_dict = json.loads(l)
             raw_collector.append(tmp_dict.copy())
             ev_name = tmp_dict.pop('EVENT_NAME', 'no-event')
-            tmp_dict = reduce_event(geo_mapping, city_mapping, tmp_dict)
+            tmp_dict = reduce_event(mapping_geo, mapping_city, mapping_refferer, tmp_dict)
             if ev_name in ["screen_view", "page_view"]:
                 view_collector.append(tmp_dict)
                 # file_reduced_views.writerow(tmp_dict)
@@ -199,10 +133,15 @@ for file_name in file_list:
 df_views = pd.DataFrame(view_collector)
 df_interactions = pd.DataFrame(interaction_collector)
 df_raw = pd.DataFrame(raw_collector)
-df_views.to_csv("data_dpg_testdata/reduced_views2.csv", index=None)
-df_interactions.to_csv("data_dpg_testdata/reduced_interactions2.csv", index=None)
-df_raw.to_csv("data_dpg_testdata/raw2.csv", index=None)
+df_views.to_csv("data_dpg_testdata/preflight/reduced_views.csv", index=None)
+df_interactions.to_csv("data_dpg_testdata/preflight/reduced_interactions.csv", index=None)
+df_raw.to_csv("data_dpg_testdata/preflight/raw.csv", index=None)
+data = bytes(df_views.to_csv(index=None), encoding="utf-8")
+s_out = gzip.compress(data)
+io.open("data_dpg_testdata/preflight/reduced_views.csv.gz", mode="wb").write(s_out)
+
 # %%
+# Full run
 limit = None
 update_freq = 10000
 for file_name in file_list:
@@ -217,33 +156,19 @@ for file_name in file_list:
                 content = TextIOWrapper(gzipfile)
                 for cnt, l in enumerate(content):
                     tmp_dict = json.loads(l)
-                    # raw_collector.append(tmp_dict.copy())
                     ev_name = tmp_dict.pop('EVENT_NAME', 'no-event')
-                    tmp_dict = reduce_event(geo_mapping, city_mapping, tmp_dict)
+                    tmp_dict = reduce_event(mapping_geo, mapping_city, tmp_dict)
                     if ev_name in ["screen_view", "page_view"]:
-                        # view_collector.append(tmp_dict)
                         writer_reduced_views.writerow(tmp_dict)
                     else:
-                        # interaction_collector.append(tmp_dict)            
                         writer_reduced_interactions.writerow(tmp_dict)
                     counter[ev_name] += 1
                     if ((cnt+1)%update_freq) == 0:
                         pbar.update(update_freq)
-                    # if cnt > limit:
-                    #     break
-
     pbar.close()        
 counter
 # %%
-
-json.dump(geo_mapping, io.open("data_dpg_testdata/reduced/mapping_geo.json", "w"))
-json.dump(city_mapping, io.open("data_dpg_testdata/reduced/mapping_city.json", "w"))
-# %%
-data = bytes(df_views.to_csv(index=None), encoding="utf-8")
-s_out = gzip.compress(data)
-io.open("data_dpg_testdata/reduced/reduced_views.csv.gz", mode="wb").write(s_out)
-# %%
-for col in df_views.columns:
-    print(f"---------- Column {col} -----------")
-    display(df_views[col].value_counts())
-
+# Save all the mappings
+json.dump(mapping_geo, io.open("data_dpg_testdata/reduced/mapping_geo.json", "w"))
+json.dump(mapping_city, io.open("data_dpg_testdata/reduced/mapping_city.json", "w"))
+json.dump(mapping_refferer, io.open("data_dpg_testdata/reduced/mapping_refferer.json", "w"))
