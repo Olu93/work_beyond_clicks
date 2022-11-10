799d11252497263bfbb49b242bfc8275de5e283d
Olusanmi Hundogan <o.hundogan@gmail.com>
2022-11-10 14:20:42 +0100

    Added questions

---
+++ analyse_dpg_data.py
@@ -0,0 +1,26 @@
+# Group Device
+# Q01: Is there a correlation between device and length of article 
+# Q02: Is there a correlation between device and time of consumption 
+# Q03: Is there a correlation between device and topic 
+# Q04: Is there a correlation between device and topic-diversity 
+# Q05: Is there a correlation between device and intent/userneeds 
+# Q06: Is there a correlation between device and location 
+
+# Group Location
+# Q01: Is there a relationship between location and topics
+# Q02: Is there a relationship between location and places mentionend in articles (named-entitities)
+
+# Group Interaction 
+# Q01: Do certain topics correlate with higher interaction
+# Q02: Which articles have certain types of interactions
+
+# Group Authors
+# Q01: Do certain authors appeal to any particluar device?  Is there any correlation between author and device-type.
+
+# Group Users
+# Q01: Is the reading pattern of a user predictable or not
+# Q02: How many power users does nu.nl have
+# Q03: What are profiles of users in general (power user, occational user)
+# - Clustering of usage by time sequence
+# - Clustering by other information such as interactions, region
+# Q04: Correlation of user profiles with topics 
